bda0c19351e915938fd81812910424b512897807
Jenifer <jenivazquezrey@gmail.com>
2026-05-19 10:46:34 +0200

    Commit inicial: Configuracion basica del proyecto con Maven y Spring Boot

---
+++ src/main/java/com/quizcode/QuizcodeApplication.java
@@ -0,0 +1,14 @@
+package com.quizcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
+
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+public class QuizcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizcodeApplication.class, args);
+	}
+
+}
